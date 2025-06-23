e946b3843ff628b6ebba6f958c0250000235a439
Serhat TOPALOĞLU <bos@unknownfacd64181a73.home>
2025-06-23 22:47:42 +0300

    Canlıya alma öncesi commit

--- main.py
+++ main.py
@@ -2,14 +2,20 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import os
 
 app = FastAPI()
 
-# Static ve templates klasörlerini bağla
+# Static dosyaları bağla
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-# Ana sayfa route'u
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
+
+# Uvicorn sadece local çalıştırma için
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 10000))
+    uvicorn.run("avukatajanda.main:app", host="0.0.0.0", port=port, reload=True)
